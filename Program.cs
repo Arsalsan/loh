614cdc5b0b1eb152f7b11b6af692c34c8c1d9b6f
Arsalan <56710640+Arsalsan@users.noreply.github.com>
2019-10-18 13:30:32 +0800

    МММ круто
    Работает отлично !

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+
+            Console.WriteLine("Введите второе число");
+            string S2 = Console.ReadLine();
+            int b = int.Parse(S2);
+
+            int c = a + b;
+
+            Console.WriteLine(a.ToString() + " + " + b.ToString() + " = " + c.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
